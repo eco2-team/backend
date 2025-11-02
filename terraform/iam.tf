6b48c4db68cf6228bc10a7d40c65f4986baab51c
mangowhoiscloud <ryoo0504@gmail.com>
2025-11-02 14:20:47 +0900

    fix: Add EBS CSI Driver IAM permissions for dynamic volume provisioning
    Added inline policy to prod-k8s-ec2-ssm-role with required permissions:
    - ec2:CreateVolume, DeleteVolume (volume lifecycle)
    - ec2:AttachVolume, DetachVolume (volume mounting)
    - ec2:DescribeVolumes, DescribeVolumeStatus (volume info)
    - ec2:CreateSnapshot, DeleteSnapshot (backup)
    - ec2:ModifyVolume (volume expansion)
    - ec2:CreateTags, DescribeTags (tagging)
    - ec2:DescribeInstances (instance info)
    
    This fixes the UnauthorizedOperation error when EBS CSI Driver
    tries to create PersistentVolumes for RabbitMQ and Prometheus.
    
    After terraform apply, PVCs will be successfully provisioned.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -34,6 +34,40 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+# EBS CSI Driver Policy (for dynamic volume provisioning)
+resource "aws_iam_role_policy" "ebs_csi_driver" {
+  name = "${var.environment}-k8s-ebs-csi-driver-policy"
+  role = aws_iam_role.ec2_ssm_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateVolume",
+          "ec2:DeleteVolume",
+          "ec2:AttachVolume",
+          "ec2:DetachVolume",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeVolumeStatus",
+          "ec2:DescribeVolumeAttribute",
+          "ec2:CreateSnapshot",
+          "ec2:DeleteSnapshot",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeSnapshotAttribute",
+          "ec2:ModifyVolume",
+          "ec2:DescribeVolumesModifications",
+          "ec2:CreateTags",
+          "ec2:DescribeTags",
+          "ec2:DescribeInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 # Instance Profile
 resource "aws_iam_instance_profile" "k8s" {
   name = "${var.environment}-k8s-instance-profile"
