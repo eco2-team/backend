6a8521f682c2e663a6cafab9f30a763474de27e5
mangowhoiscloud <ryoo0504@gmail.com>
2025-12-25 01:27:01 +0900

    fix(celery): import dlq_tasks in __init__.py for autodiscover
    autodiscover_tasks는 기본적으로 tasks.py만 찾음.
    dlq_tasks.py의 태스크들이 worker에 등록되지 않아
    'KeyError: dlq.reprocess_my_reward' 발생.
    
    __init__.py에서 명시적 import로 해결.

--- domains/_shared/celery/__init__.py
+++ domains/_shared/celery/__init__.py
@@ -7,6 +7,10 @@ Celery Shared Module
 from domains._shared.celery.base_task import BaseTask, WebhookTask
 from domains._shared.celery.config import CelerySettings, get_celery_settings
 
+# DLQ 재처리 태스크 등록 (autodiscover_tasks는 tasks.py만 찾음)
+# 각 worker가 이 모듈을 import할 때 자동으로 DLQ 태스크들이 등록됨
+from domains._shared.celery import dlq_tasks as _dlq_tasks  # noqa: F401
+
 __all__ = [
     "BaseTask",
     "WebhookTask",
